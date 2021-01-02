4cccd35b79192a538f611a84b47d07c3e58f74ea
thanhdev <doanmeo202@gmail.com>
2021-01-02 14:44:47 +0700

    new tempalte

---
+++ app.js
@@ -0,0 +1,37 @@
+//Define an angular module for our app
+var sampleApp = angular.module('sampleApp', []);
+
+//Define Routing for app
+//Uri /AddNewOrder -> template AddOrder.html and Controller AddOrderController
+//Uri /ShowOrders -> template ShowOrders.html and Controller AddOrderController
+sampleApp.config(['$routeProvider','$locationProvider',
+  function($routeProvider,$locationProvider) {
+      
+    $routeProvider.
+      when('/AddNewOrder', {
+	templateUrl: 'test/templates/add_order.html',
+	controller: 'AddOrderController'
+      }).
+      when('/ShowOrders', {
+	templateUrl: 'test/templates/show_orders.html',
+	controller: 'ShowOrdersController'
+      }).
+      otherwise({
+	redirectTo: '/AddNewOrder'
+      });
+      $locationProvider.html5Mode(true);
+}]);
+
+
+sampleApp.controller('AddOrderController', function($scope) {
+	
+	$scope.message = 'This is Add new order screen';
+	
+});
+
+
+sampleApp.controller('ShowOrdersController', function($scope) {
+
+	$scope.message = 'This is Show orders screen';
+
+});
